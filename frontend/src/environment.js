db0bf12392bffd048f3fef736dc7faf6bfc25365
shakir94 <loneshakir418@gmail.com>
2026-01-25 15:35:02 +0530

    changes in frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true
 const server = IS_PROD ?
-"https://shakirmeetbackend-cn9m.onrender.com" :
+"https://shakirmeetbackend-qilx.onrender.com" :
 "http://localhost:8000"
 
 export default server
