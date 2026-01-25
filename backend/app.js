3065b507fd2c79fd47cb6606fe551e508e1795a1
shakir94 <loneshakir418@gmail.com>
2026-01-25 15:27:31 +0530

    Edit in Frontend

--- backend/app.js
+++ backend/app.js
@@ -14,11 +14,12 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  }),
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   }),
+// );
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
